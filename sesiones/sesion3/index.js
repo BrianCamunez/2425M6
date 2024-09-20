aacaa5767d800ac72a47d0b70f49486038b4d228
BrianCamunez <124356373+BrianCamunez@users.noreply.github.com>
2024-09-20 17:06:35 +0000

    V1

---
+++ sesiones/sesion3/index.js
@@ -0,0 +1,6 @@
+const boton1 = document.querySelector('#btn1');
+console.log(boton1);
+const titulo = document.querySelector("h1");
+console.log(titulo);
+console.log(titulo.innerHTML);
+titulo.innerHTML = '<em>Otro Titulo</em>';
